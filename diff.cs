fdfc3a733948642304cef3b7a20cf69b8112762b
ipshita bhattacharjee <official899ips@gmail.com>
2023-12-04 15:22:35 +0530

    done

---
+++ diff.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int num1 = 5;
+        int num2 = 7;
+        int difference = num1 - num2;
+        Console.WriteLine("The difference of {0} and {1} is: {2}", num1, num2, difference);
+    }
+}
