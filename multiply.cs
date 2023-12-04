727d9f3aecb1fdf9770fd9d5cb85ab8d14239551
ipshita bhattacharjee <official899ips@gmail.com>
2023-12-04 15:33:14 +0530

    done

---
+++ multiply.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int num1 = 5;
+        int num2 = 7;
+        int product = num1 * num2;
+        Console.WriteLine("The product of {0} and {1} is: {2}", num1, num2, product);
+    }
+}
